5ed978db919214afb3ae6a53b900780c20888d6c
Vedank Purohit <VedankPurohit2@gmail.com>
2025-05-29 10:22:43 +0530

    Api server init
    has abilty to spawn containers for build using ECS

---
+++ Api Server/index.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import { generateSlug } from 'random-word-slugs';
+import { ECSClient,RunTaskCommand } from '@aws-sdk/client-ecs';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = 9000;
+const ecsClient = new ECSClient({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
+
+const config = {
+    cluster: process.env.BUILDER_CLUSTER_ARN,
+    taskDefinition: process.env.BUILDER_TASK_DEFINITION_ARN,
+}
+
+app.use(express.json())
+
+app.post('/project', async (req, res) => {
+    const {gitURL} = req.body
+    const projectSlug  = generateSlug();
+
+    const command = new RunTaskCommand({
+        cluster: config.cluster,
+        taskDefinition: config.taskDefinition,
+        launchType: 'FARGATE',
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration:{
+                assignPublicIp:"ENABLED",
+                subnets: [
+                    process.env.SUBNET_1,
+                    process.env.SUBNET_2,
+                    process.env.SUBNET_3
+                ],
+                securityGroups: [process.env.SECURITY_GROUP]
+            }
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: process.env.CONTAINER_NAME,
+                    environment: [
+                        {name: 'GIT_REPOSITORY__URL', value: gitURL},
+                        {name: 'PROJECT_ID', value: projectSlug}
+                    ]
+                }
+            ]
+        }
+
+
+    })
+
+    await ecsClient.send(command);
+    return res.json({status: "queued", data: {projectSlug,url: `http://${projectSlug}.localhost:8000`} } )
+
+
+})
+
+
+app.listen(PORT,() => console.log(`Api Server is running on http://localhost:${PORT}`));
